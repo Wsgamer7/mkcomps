c8eee31653a6d004785cbb8c4e7e6427e070866e
Shuo Wang <ws18022@outlook.com>
2025-01-01 10:19:04 +0800

    func to arrow

--- mkcomps.js
+++ mkcomps.js
@@ -9,25 +9,26 @@ if (args.length === 2) {
   console.log("Usage: node mkcomps.js <parentPath> <componentName>");
 }
 
-function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
-function createComponentFile(parentPath, componentName) {
+const capitalizeFirstLetter = (string) =>
+  string.charAt(0).toUpperCase() + string.slice(1);
+
+const createComponentFile = (parentPath, componentName) => {
   const componentPath = path.join(parentPath, componentName, "index.tsx");
   const componentContent = `import styles from './index.module.scss' \n export default function ${componentName} () {
         return (<div>${componentName}</div>)
     }`;
   fs.writeFileSync(componentPath, componentContent);
   console.log(`Component is in ${componentPath}`);
-}
-function createScssFile(parentPath, componentName) {
+};
+
+const createScssFile = (parentPath, componentName) => {
   const scssPath = path.join(parentPath, componentName, "index.module.scss");
   const cssMediaQuery = "@media (max-width: 900px) {\n}";
   fs.writeFileSync(scssPath, cssMediaQuery);
   console.log(`Scss is in ${scssPath}`);
-}
+};
 
-function createComponent(parentPath, componentName) {
+const createComponent = (parentPath, componentName) => {
   componentName = capitalizeFirstLetter(componentName);
   const componentPath = path.join(parentPath, componentName);
   if (fs.existsSync(componentPath)) {
@@ -37,4 +38,4 @@ function createComponent(parentPath, componentName) {
   fs.mkdirSync(componentPath, { recursive: true });
   createComponentFile(parentPath, componentName);
   createScssFile(parentPath, componentName);
-}
+};
